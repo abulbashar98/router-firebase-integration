e0b05bf75410927aaf257efdaefbb92e6b11cbe3
abulbashar98 <abulbashar98116@gmail.com>
2022-04-12 10:48:41 +0600

    Created React App, Installed Firebase, Installed React Router, Created a project in firebase, Registerd app, Copie firebase config to firebase.init.js, Solved React 18.0 issue, Solved web vitals issue by installing web Vitals again, Command in RADME.md

---
+++ README.md
@@ -0,0 +1,8 @@
+# Getting Started with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Web Vitals problem? Install again
+
+* Command:- npm i web-vitals --save-dev
+
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDt9_yTykdNiTV-UHgSK2enbBXMQ_dkkIg",
+    authDomain: "router-firebase-integrat-2d4e5.firebaseapp.com",
+    projectId: "router-firebase-integrat-2d4e5",
+    storageBucket: "router-firebase-integrat-2d4e5.appspot.com",
+    messagingSenderId: "999383207798",
+    appId: "1:999383207798:web:2088441943ceb1ffeb7457"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
